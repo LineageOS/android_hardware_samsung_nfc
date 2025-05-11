0e6759f3ea468a0e964ee1201ada0e9e9b34f56d
Atakan <ata.kaner03@gmail.com>
2025-05-11 11:04:57 +0300

    nfc: Initial Samsung AIDL NFC HAL
    Change-Id: Iba05fa24c655769c20ddddd4caba506688919fce

--- halimpl/include/hal.h
+++ halimpl/include/hal.h
@@ -18,14 +18,10 @@
 #define __NFC_SEC_HAL__
 
 #include <hardware/nfc.h>
+#include <cstdio>
 
 #include "hal_msg.h"
 #include "osi.h"
-
-#include "android/hardware/nfc/1.2/INfc.h"
-#include "android/hardware/nfc/1.2/types.h"
-
-using ::android::hardware::nfc::V1_1::NfcConfig;
 
 #ifndef __bool_true_false_are_defined
 #define __bool_true_false_are_defined
@@ -157,15 +153,14 @@ void nfc_hal_deinit(void);
 int nfc_hal_open(nfc_stack_callback_t* p_cback,
                  nfc_stack_data_callback_t* p_data_cback);
 int nfc_hal_write(uint16_t data_len, const uint8_t* p_data);
-int nfc_hal_core_initialized(uint8_t* p_core_init_rsp_params);
+int nfc_hal_core_initialized();
 int nfc_hal_pre_discover();
 int nfc_hal_close();
 int nfc_hal_control_granted();
 int nfc_hal_power_cycle();
-// HIDL INfc 1.1
 int nfc_hal_factory_reset(void);
-int nfc_hal_closeForPowerOffCase(void);
-void nfc_hal_getVendorConfig(NfcConfig& config);  // To-Do
+void nfc_hal_setLogging(bool enable);
+bool nfc_hal_isLoggingEnabled();
 
 /*************************************
  * NFC HAL functions.
