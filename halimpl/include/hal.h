db218a6b97c41b5d5366859a1e9dc534b78ce166
Atakan <ata.kaner03@gmail.com>
2025-05-11 17:04:49 +0300

    nfc: Initial Samsung AIDL NFC HAL
    - Based on hardware/nxp/nfc/snxxx | android-15.0.0_r32
    - Required changes applied from prebuilt halimpl of r8q and pa1q.
    
    Change-Id: Iba05fa24c655769c20ddddd4caba506688919fce

--- halimpl/include/hal.h
+++ halimpl/include/hal.h
@@ -300,6 +300,7 @@ int nfc_hal_open(nfc_stack_callback_t* p_cback,
                  nfc_stack_data_callback_t* p_data_cback);
 int nfc_hal_write(uint16_t data_len, const uint8_t* p_data);
 int nfc_hal_core_initialized(uint8_t* p_core_init_rsp_params);
+int nfc_hal_core_initialized_for_aidl();
 int nfc_hal_pre_discover();
 int nfc_hal_close();
 int nfc_hal_control_granted();
@@ -308,6 +309,8 @@ int nfc_hal_power_cycle();
 int nfc_hal_factory_reset(void);
 int nfc_hal_closeForPowerOffCase(void);
 void nfc_hal_getVendorConfig(NfcConfig& config);  // To-Do
+void nfc_hal_setLogging(bool enable);
+bool nfc_hal_isLoggingEnabled();
 
 /*************************************
  * NFC HAL functions.
